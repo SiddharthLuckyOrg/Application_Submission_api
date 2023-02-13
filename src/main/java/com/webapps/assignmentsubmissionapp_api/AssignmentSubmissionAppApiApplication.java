fd72385eb39caa68292a15a53ccb4a4c9f987f47
Siddharth Lucky <siddharth.luckyreddy@gmail.com>
2023-02-13 11:04:40 -0500

    - Initial commit, defined spring security and db properties

---
+++ src/main/java/com/webapps/assignmentsubmissionapp_api/AssignmentSubmissionAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.webapps.assignmentsubmissionapp_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentSubmissionAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentSubmissionAppApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webapps/assignmentsubmissionapp_api/AssignmentSubmissionAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapps.assignmentsubmissionapp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentSubmissionAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
